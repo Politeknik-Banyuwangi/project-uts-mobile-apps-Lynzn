83eded2d4c7b39731a282398fc5c138ffdf5f2ae
FlutLab.io <>
2022-05-31 05:16:50 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -17,9 +17,11 @@ class Chats extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
+            //Memanggil objek chatCard
             chatCard("Gojo Satoru", "Your gift has been received", "01:19", Icons.done_all, Colors.blue, NetworkImage('https://www.greenscene.co.id/wp-content/uploads/2021/09/Jujutsu-Kaisen-4.jpg')),
             chatCard("Kamisato Ayato", "Let's meet up at usual place", "23:49", Icons.done_all, Colors.grey, NetworkImage('https://gamerwk.sgp1.cdn.digitaloceanspaces.com/2022/03/Ayato-Best-Build.jpg')),
             chatCard("Kaedehara Kazuha", "Hello...", "22:19", Icons.done, Colors.grey, NetworkImage('https://pbs.twimg.com/profile_images/1441938154080985088/q1ejAf4n_400x400.jpg')),
+            chatCard("Kamisato Ayaka", "Hiya ", "19:51", Icons.done_all, Colors.grey, NetworkImage('https://foto.kontan.co.id/eQpBGeMMmMkhHSwU5oO1RAn-Gjw=/smart/2021/07/15/2100661850p.jpg')),
           ],
         ),
       ),
@@ -34,6 +36,7 @@ class Chats extends StatelessWidget {
     );
   }
 
+  //Membuat chatCard
   Widget chatCard(String name, String desk, String time, IconData iconD, Color iconC, NetworkImage ntimg) {
     return Card(
       margin: EdgeInsets.only(bottom: 0.9),
