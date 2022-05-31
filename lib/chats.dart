442941b6b93624e18dc37d01cdeb8eedae78da14
FlutLab.io <>
2022-05-31 05:00:05 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -17,7 +17,8 @@ class Chats extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-            chatCard("Gojo Satoru", "Aku butuh", "17:19", Icons.done_all, Colors.blue, NetworkImage('https://www.greenscene.co.id/wp-content/uploads/2021/09/Jujutsu-Kaisen-4.jpg')),
+            chatCard("Gojo Satoru", "I miss u", "17:19", Icons.done_all, Colors.blue, NetworkImage('https://www.greenscene.co.id/wp-content/uploads/2021/09/Jujutsu-Kaisen-4.jpg')),
+            chatCard("Kamisato Ayato", "Let's meet up at usual place", "08:49", Icons.done_all, Colors.grey, NetworkImage('https://gamerwk.sgp1.cdn.digitaloceanspaces.com/2022/03/Ayato-Best-Build.jpg')),
           ],
         ),
       ),
